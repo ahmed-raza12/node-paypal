cc45e2136865abdc94886f9658cca018560c089c
unknown <ar907080@gmail.com>
2019-10-19 13:19:39 +0500

    go plz

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.get("/paypal", (req, res) => {
             payment_method: "paypal"
         },
         redirect_urls: {
-            return_url: "http://localhost:3000/success",
-            cancel_url: "http://localhost:3000/cancel"
+            return_url: "https://paypal-rn.herokuapp.com/success",
+            cancel_url: "https://paypal-rn.herokuapp.com/cancel"
         },
         transactions: [
             {
