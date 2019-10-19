044682de1f5d6971df627867447e55731aff35de
unknown <ar907080@gmail.com>
2019-10-19 11:58:26 +0500

    make it better

--- app.js
+++ app.js
@@ -435,6 +435,6 @@ paypal.billingPlan.create(billingPlanAttributes, function (error, billingPlan) {
 
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running");
 });
