d1f190ae0503b496173fea1575371affcae1af9a
unknown <ar907080@gmail.com>
2019-10-24 12:45:26 +0500

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const engines = require("consolidate");
 const paypal = require("paypal-rest-sdk");
+var mysql = require('mysql');
 
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "crypto"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 // const qs = require("querystring");
 // const http = require("https");
 
@@ -31,7 +44,12 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/package2", (req, res) => {
+    res.render("package2");
+});
+
 app.get("/paypal", (req, res) => {
+    console.log(req, 'reqqq')
     var create_payment_json = {
         intent: "sale",
         payer: {
@@ -48,7 +66,7 @@ app.get("/paypal", (req, res) => {
                         {
                             name: "item",
                             sku: "item",
-                            price: "1.00",
+                            price: req.price,
                             currency: "USD",
                             quantity: 1
                         }
@@ -63,7 +81,7 @@ app.get("/paypal", (req, res) => {
         ]
     };
 
-    paypal.payment.create(create_payment_json, function(error, payment) {
+    paypal.payment.create(create_payment_json, function (error, payment) {
         if (error) {
             throw error;
         } else {
@@ -90,7 +108,7 @@ app.get("/success", (req, res) => {
         ]
     };
 
-    paypal.payment.execute(paymentId, execute_payment_json, function(
+    paypal.payment.execute(paymentId, execute_payment_json, function (
         error,
         payment
     ) {
@@ -109,8 +127,176 @@ app.get("cancel", (req, res) => {
     res.render("cancel");
 });
 
-app.get("/create-plan", (req, res) => {
-        var billingPlanAttributes = {
+
+
+app.post("/create-plan", (req, res) => {
+    console.log(req.body, 'req')
+    var billingPlanAttributes = {
+        "description": "Create Plan for Regular",
+        "merchant_preferences": {
+            "auto_bill_amount": "yes",
+            "cancel_url": "http://www.cancel.com",
+            "initial_fail_amount_action": "continue",
+            "max_fail_attempts": "1",
+            "return_url": "http://localhost:3000/success",
+            "setup_fee": {
+                "currency": "USD",
+                "value": "25"
+            }
+        },
+        "name": "Testing1-Regular1",
+        "payment_definitions": [
+            {
+                "amount": {
+                    "currency": "USD",
+                    "value": req.body.value,
+                },
+                "charge_models": [
+                    {
+                        "amount": {
+                            "currency": "USD",
+                            "value": "10.60"
+                        },
+                        "type": "SHIPPING"
+                    },
+                    {
+                        "amount": {
+                            "currency": "USD",
+                            "value": "20"
+                        },
+                        "type": "TAX"
+                    }
+                ],
+                "cycles": "0",
+                "frequency": "MONTH",
+                "frequency_interval": "1",
+                "name": "Regular 1",
+                "type": "REGULAR"
+            },
+            {
+                "amount": {
+                    "currency": "USD",
+                    "value": "20"
+                },
+                "charge_models": [
+                    {
+                        "amount": {
+                            "currency": "USD",
+                            "value": "10.60"
+                        },
+                        "type": "SHIPPING"
+                    },
+                    {
+                        "amount": {
+                            "currency": "USD",
+                            "value": "20"
+                        },
+                        "type": "TAX"
+                    }
+                ],
+                "cycles": "4",
+                "frequency": "MONTH",
+                "frequency_interval": "1",
+                "name": "Trial 1",
+                "type": "TRIAL"
+            }
+        ],
+        "type": "INFINITE"
+    };
+
+    paypal.billingPlan.create(billingPlanAttributes, function (error, billingPlan) {
+        if (error) {
+            console.log(error);
+            throw error;
+        } else {
+            console.log("Create Billing Plan Response", billingPlan.id);
+            // res.send(billingPlan)
+            var amount = parseInt(req.body.value);
+            console.log(amount, 'amou');
+            // INSERT INTO `trader_paypal`(`trader_id`, `user_id`, `package_name`, `amout`, `plan_id`)  (5, 'basic', "${amount}", "${billingPlan.id}")
+            var sql = `INSERT INTO trader_paypal(user_id, package_name, amount, plan_id) VALUES (5, 'basic', ${amount}, "${billingPlan.id}")`;
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("new record inserted", result);
+
+            });
+            //    mysqli_query(con, insert)
+            // res.render("package2");
+            // window.location.url = "/package20"
+
+        }
+    });
+
+});
+
+
+
+app.get("/get-plan", (req, res) => {
+
+    var user_plans = `SELECT * FROM trader_paypal WHERE user_id = 5`
+    // console.log(user_plans, 'user')
+    con.query(user_plans, function (err, result) {
+        if (err) throw err;
+        console.log("new record inserted", result);
+        res.send(result)
+        // var allId = result.map((id) => {
+        //     return id.user_id
+        // })
+        // console.log(allId, 'all id')
+        // paypal.billingPlan.get(billingPlanId, function (error, result) {
+        //     if (error) {
+        //         console.log(error);
+        //         throw error;
+        //     } else {
+        //         console.log("Get Billing Plan");
+        //         res.send(JSON.stringify(billingPlan));
+
+        //     }
+        // });
+    });
+
+
+});
+
+
+app.get("/get-plans", (req, res) => {
+    // var data = `SELECT * FROM trader_paypal user_id = 5`
+    // con.query(data, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("new record inserted", result);
+    // });
+    // console.log()
+    var list_billing_plan = {
+        'status': 'ACTIVE',
+        'page_size': 5,
+        'page': 1,
+        'total_required': 'yes'
+    };
+
+    paypal.billingPlan.list(list_billing_plan, function (error, billingPlan) {
+        if (error) {
+            throw error;
+        } else {
+            console.log("List Billing Plans Response");
+            res.send(JSON.stringify(billingPlan));
+        }
+    });
+
+
+
+});
+
+
+
+app.get("/abc", (req, res) => {
+
+
+
+    var isoDate = new Date();
+    isoDate.setSeconds(isoDate.getSeconds() + 4);
+    isoDate.toISOString().slice(0, 19) + 'Z';
+
+    var billingPlanAttributes = {
         "description": "Create Plan for Regular",
         "merchant_preferences": {
             "auto_bill_amount": "yes",
@@ -183,220 +369,80 @@ app.get("/create-plan", (req, res) => {
         "type": "INFINITE"
     };
 
+    var billingPlanUpdateAttributes = [
+        {
+            "op": "replace",
+            "path": "/",
+            "value": {
+                "state": "ACTIVE"
+            }
+        }
+    ];
+
+    var billingAgreementAttributes = {
+        "name": "Fast Speed Agreement",
+        "description": "Agreement for Fast Speed Plan",
+        "start_date": isoDate,
+        "plan": {
+            "id": "P-3VS43194KX216543UVM7HUXI"
+        },
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "shipping_address": {
+            "line1": "StayBr111idge Suites",
+            "line2": "Cro12ok Street",
+            "city": "San Jose",
+            "state": "CA",
+            "postal_code": "95112",
+            "country_code": "US"
+        }
+    };
+
+    // Create the billing plan
     paypal.billingPlan.create(billingPlanAttributes, function (error, billingPlan) {
         if (error) {
             console.log(error);
             throw error;
         } else {
             console.log("Create Billing Plan Response");
-            res.send(billingPlan)
+            console.log(billingPlan);
 
-            
-        }
-    });
+            // Activate the plan by changing status to Active
+            paypal.billingPlan.update(billingPlan.id, billingPlanUpdateAttributes, function (error, response) {
+                if (error) {
+                    console.log(error);
+                    throw error;
+                } else {
+                    console.log("Billing Plan state changed to " + billingPlan.state);
+                    billingAgreementAttributes.plan.id = billingPlan.id;
 
-});
+                    // Use activated billing plan to create agreement
+                    paypal.billingAgreement.create(billingAgreementAttributes, function (error, billingAgreement) {
+                        if (error) {
+                            console.log(error);
+                            throw error;
+                        } else {
+                            console.log("Create Billing Agreement Response");
+                            //console.log(billingAgreement);
+                            for (var index = 0; index < billingAgreement.links.length; index++) {
+                                if (billingAgreement.links[index].rel === 'approval_url') {
+                                    var approval_url = billingAgreement.links[index].href;
+                                    console.log("For approving subscription via Paypal, first redirect user to");
+                                    console.log(approval_url);
 
-
-app.get("/get-plan", (req, res) => {
-
-        var billingPlanId = "P-3VS43194KX216543UVM7HUXI";
-
-        paypal.billingPlan.get(billingPlanId, function (error, billingPlan) {
-            if (error) {
-                console.log(error);
-                throw error;
-            } else {
-                console.log("Get Billing Plan");
-                res.send(JSON.stringify(billingPlan));
-
-
-            }
-    });
-
-
-});
-
-
-app.get("/get-plans", (req, res) => {
-
-    var list_billing_plan = {
-        'status': 'ACTIVE',
-        'page_size': 5,
-        'page': 1,
-        'total_required': 'yes'
-    };
-
-    paypal.billingPlan.list(list_billing_plan, function (error, billingPlan) {
-        if (error) {
-            throw error;
-        } else {
-            console.log("List Billing Plans Response");
-            res.send(JSON.stringify(billingPlan));
-        }
-    });
-            
-
-
-});
-
-
-
-app.get("/abc",(req,res)=>{
-
-
-
-var isoDate = new Date();
-isoDate.setSeconds(isoDate.getSeconds() + 4);
-isoDate.toISOString().slice(0, 19) + 'Z';
-
-var billingPlanAttributes = {
-    "description": "Create Plan for Regular",
-    "merchant_preferences": {
-        "auto_bill_amount": "yes",
-        "cancel_url": "http://www.cancel.com",
-        "initial_fail_amount_action": "continue",
-        "max_fail_attempts": "1",
-        "return_url": "http://www.success.com",
-        "setup_fee": {
-            "currency": "USD",
-            "value": "25"
-        }
-    },
-    "name": "Testing1-Regular1",
-    "payment_definitions": [
-        {
-            "amount": {
-                "currency": "USD",
-                "value": "100"
-            },
-            "charge_models": [
-                {
-                    "amount": {
-                        "currency": "USD",
-                        "value": "10.60"
-                    },
-                    "type": "SHIPPING"
-                },
-                {
-                    "amount": {
-                        "currency": "USD",
-                        "value": "20"
-                    },
-                    "type": "TAX"
-                }
-            ],
-            "cycles": "0",
-            "frequency": "MONTH",
-            "frequency_interval": "1",
-            "name": "Regular 1",
-            "type": "REGULAR"
-        },
-        {
-            "amount": {
-                "currency": "USD",
-                "value": "20"
-            },
-            "charge_models": [
-                {
-                    "amount": {
-                        "currency": "USD",
-                        "value": "10.60"
-                    },
-                    "type": "SHIPPING"
-                },
-                {
-                    "amount": {
-                        "currency": "USD",
-                        "value": "20"
-                    },
-                    "type": "TAX"
-                }
-            ],
-            "cycles": "4",
-            "frequency": "MONTH",
-            "frequency_interval": "1",
-            "name": "Trial 1",
-            "type": "TRIAL"
-        }
-    ],
-    "type": "INFINITE"
-};
-
-var billingPlanUpdateAttributes = [
-    {
-        "op": "replace",
-        "path": "/",
-        "value": {
-            "state": "ACTIVE"
-        }
-    }
-];
-
-var billingAgreementAttributes = {
-    "name": "Fast Speed Agreement",
-    "description": "Agreement for Fast Speed Plan",
-    "start_date": isoDate,
-    "plan": {
-        "id": "P-3VS43194KX216543UVM7HUXI"
-    },
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "shipping_address": {
-        "line1": "StayBr111idge Suites",
-        "line2": "Cro12ok Street",
-        "city": "San Jose",
-        "state": "CA",
-        "postal_code": "95112",
-        "country_code": "US"
-    }
-};
-
-// Create the billing plan
-paypal.billingPlan.create(billingPlanAttributes, function (error, billingPlan) {
-    if (error) {
-        console.log(error);
-        throw error;
-    } else {
-        console.log("Create Billing Plan Response");
-        console.log(billingPlan);
-
-        // Activate the plan by changing status to Active
-        paypal.billingPlan.update(billingPlan.id, billingPlanUpdateAttributes, function (error, response) {
-            if (error) {
-                console.log(error);
-                throw error;
-            } else {
-                console.log("Billing Plan state changed to " + billingPlan.state);
-                billingAgreementAttributes.plan.id = billingPlan.id;
-
-                // Use activated billing plan to create agreement
-                paypal.billingAgreement.create(billingAgreementAttributes, function (error, billingAgreement) {
-                    if (error) {
-                        console.log(error);
-                        throw error;
-                    } else {
-                        console.log("Create Billing Agreement Response");
-                        //console.log(billingAgreement);
-                        for (var index = 0; index < billingAgreement.links.length; index++) {
-                            if (billingAgreement.links[index].rel === 'approval_url') {
-                                var approval_url = billingAgreement.links[index].href;
-                                console.log("For approving subscription via Paypal, first redirect user to");
-                                console.log(approval_url);
-
-                                console.log("Payment token is");
-                                console.log(url.parse(approval_url, true).query.token);
-                                // See billing_agreements/execute.js to see example for executing agreement 
-                                // after you have payment token
+                                    console.log("Payment token is");
+                                    console.log(url.parse(approval_url, true).query.token);
+                                    // See billing_agreements/execute.js to see example for executing agreement 
+                                    // after you have payment token
+                                }
                             }
                         }
-                    }
-                });
-            }
-        });
-    }
-});
+                    });
+                }
+            });
+        }
+    });
 
 })
 
